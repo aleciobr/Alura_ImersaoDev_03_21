f0dfc5adc04195aaf91550430d337bf302ef26b0
Bruno Alécio <50933242+aleciobr@users.noreply.github.com>
2021-09-19 15:12:14 -0300

    up

--- master.js
+++ master.js
@@ -1,20 +1,26 @@
 var numeroAle = parseInt(Math.random() * 11);
-
+var tentativas = 0;
 function Chutar(){
     var chute = parseInt(document.getElementById("valor").value);
     var resultadoChute = document.getElementById("resultado");
+    var chance = document.getElementById("chance")
+    tentativas += 1;
     if(chute == numeroAle){
+        
         var acertou = "Você acerto o numero "+ numeroAle;
         resultadoChute.innerHTML = acertou;
-    
+        var cha = "Você tentou " + tentativas;
+        chance.innerHTML = cha;
     }else if(chute >= 11 || chute < 0){
         var certo ="Digite um numero entre 0 e 10!";
         resultadoChute.innerHTML = certo;
-    }else{
-        var errou = "Você errou o numero";
-        resultadoChute.innerHTML = errou;
+    }else if(chute < numeroAle){
+        var errou = "Você errou o numero é maior";
+        resultadoChute.innerHTML = errou;    
+    }else if(chute > numeroAle){
+        var errou = "Você errou o numero é menor";
+        resultadoChute.innerHTML = errou;     
     }
-
 }
 
 // O == ele pode retorna um valor com aspas tipo 10 e "10" é considerado valores iguais
